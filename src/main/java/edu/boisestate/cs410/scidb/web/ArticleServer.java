fcc79b5703dc1d8554d5aec42d715eb31ea3eaef
Michael Ekstrand <michaelekstrand@boisestate.edu>
2016-10-18 15:58:33 -0600

    pub lists

--- src/main/java/edu/boisestate/cs410/scidb/web/ArticleServer.java
+++ src/main/java/edu/boisestate/cs410/scidb/web/ArticleServer.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import spark.*;
 import spark.template.pebble.PebbleTemplateEngine;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,11 +49,56 @@ public class ArticleServer {
             fields.put("publications", PubListEntry.retrieve(cxn));
         }
 
-        return new ModelAndView(fields, "home.html");
+        return new ModelAndView(fields, "home.html.twig");
     }
 
-    public ModelAndView pubPage(Request request, Response response) {
-        return null;
+    public ModelAndView pubPage(Request request, Response response) throws SQLException {
+        long pid = Long.parseLong(request.params("pid"));
+
+        Map<String,Object> fields = new HashMap<>();
+
+        try (Connection cxn = pool.getConnection()) {
+            // look up the pub info
+            try (PreparedStatement ps = cxn.prepareStatement("SELECT pub_title FROM publication WHERE pub_id = ?")) {
+                ps.setLong(1, pid);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        fields.put("title", rs.getString("pub_title"));
+                    } else {
+                        http.halt(404, "No such publication " + pid);
+                    }
+                }
+            }
+
+            // grab the issues
+            try (PreparedStatement ps = cxn.prepareStatement("SELECT issue_id, iss_title, iss_volume, iss_number, iss_date FROM issue WHERE pub_id = ? ORDER BY iss_date")) {
+                ps.setLong(1, pid);
+                try (ResultSet rs = ps.executeQuery()) {
+                    List<Map<String,Object>> issues = new ArrayList<>();
+                    while (rs.next()) {
+                        Map<String,Object> issue = new HashMap<>();
+                        issue.put("id", rs.getLong("issue_id"));
+                        issue.put("title", rs.getString("iss_title"));
+                        if (rs.wasNull()) {
+                            issue.put("title", null);
+                        }
+                        issue.put("volume", rs.getInt("iss_volume"));
+                        if (rs.wasNull()) {
+                            issue.put("volume", null);
+                        }
+                        issue.put("number", rs.getInt("iss_number"));
+                        if (rs.wasNull()) {
+                            issue.put("number", null);
+                        }
+                        issue.put("date", rs.getDate("iss_date"));
+                        issues.add(issue);
+                    }
+                    fields.put("issues", issues);
+                }
+            }
+        }
+
+        return new ModelAndView(fields, "pub.html.twig");
     }
 
 }
