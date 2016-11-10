939f134cf4d6d7d805ce27b4c3f03b3436e07d1e
Michael Ekstrand <michaelekstrand@boisestate.edu>
2016-11-10 10:14:58 -0700

    docker improvements

--- initdb.sh
+++ initdb.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-if test -z "$1"; then
-    echo "no database init file" >&2
+DBNAME="$1"; shift
+if test -z "$DBNAME"; then
+    echo "no database name" >&2
+    exit 2
 fi
 
 # create a Postgres database
-initdb --auth=trust /srv/psql
+initdb --auth=trust /srv/psql || exit 2
 
 # start up the database
-pg_ctl start -w -D /srv/psql
+pg_ctl start -w -D /srv/psql || exit 2
+
+createdb "$DBNAME" || exit 2
 
 # and load data
 for DBFILE in "$@"; do
     echo "Loading from $DBFILE" >&2
-    psql -f "$DBFILE"
+    psql -f "$DBFILE" "$DBNAME" || exit 2
 done
